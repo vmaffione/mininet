709a548cc3377f6dac588ead712fff5c65f3bde3
Vincenzo Maffione <v.maffione@gmail.com>
2014-12-09 23:54:56 +0100

    two-sites network + public network + ISP network

--- custom/script2.py
+++ custom/script2.py
@@ -19,25 +19,53 @@ def addLinkWithIP(net, node1, ip1, node2, ip2):
 
 def main():
     net = Mininet()
-    # Create hosts. We specify ip = None, so that Mininet
-    # does not automatically configure the IPs
-    h1 = net.addHost('h1', ip = None)
+    # Create hosts and switches. For hosts, we specify ip = None, so that
+    # Mininet does not automatically configure the IPs. The switches are
+    # implemented by standard linux software bridges.
+    h1 = net.addHost('h1', ip = None) # CUstomer Edge A1
     h2 = net.addHost('h2', ip = None)
     h3 = net.addHost('h3', ip = None)
-    h4 = net.addHost('h4', ip = None)
-    h5 = net.addHost('h5', ip = None)
+    s1 = net.addSwitch('s1', lxbr.LinuxBridge) # Customer A1 switch
 
-    # Create switches, using the standard linux bridge
-    s1 = net.addSwitch('s1', lxbr.LinuxBridge)
-    s2 = net.addSwitch('s2', lxbr.LinuxBridge)
+    h11 = net.addHost('h11', ip = None) # Customer Edge A2
+    h12 = net.addHost('h12', ip = None)
+    h13 = net.addHost('h13', ip = None)
+    s2 = net.addSwitch('s2', lxbr.LinuxBridge) # Customer A2 switch
+
+    h21 = net.addHost('h21', ip = None) # Public networks Edge
+    h22 = net.addHost('h22', ip = None)
+    h23 = net.addHost('h23', ip = None)
+    s3 = net.addSwitch('s3', lxbr.LinuxBridge) # Public network switch
+
+    h91 = net.addHost('h91', ip = None) # Provider Edge A1
+    h92 = net.addHost('h92', ip = None) # Provider Edge A2
+    h93 = net.addHost('h93', ip = None) # Provider Edge public network
+    s4 = net.addSwitch('s4', lxbr.LinuxBridge) # ISP switch
 
     # Create links, and assign IP to the host interfaces
-    addLinkWithIP(net, h1, '10.0.1.1/24', h2, '10.0.1.2/24')
-    addLinkWithIP(net, h2, '192.168.0.2/24', s1, None)
-    addLinkWithIP(net, s1, None, h3, '192.168.0.3/24')
-    addLinkWithIP(net, h3, '10.0.2.3/24', s2, None)
-    addLinkWithIP(net, s2, None, h4, '10.0.2.4/24')
-    addLinkWithIP(net, s2, None, h5, '10.0.2.5/24')
+
+    # A1 network
+    addLinkWithIP(net, h1, '10.0.1.1/24', s1, None)
+    addLinkWithIP(net, h2, '10.0.1.2/24', s1, None)
+    addLinkWithIP(net, h3, '10.0.1.3/24', s1, None)
+    addLinkWithIP(net, h1, '192.168.81.1/24', h91, '192.168.81.91/24')
+
+    # A2 network
+    addLinkWithIP(net, h11, '10.0.2.11/24', s2, None)
+    addLinkWithIP(net, h12, '10.0.2.12/24', s2, None)
+    addLinkWithIP(net, h13, '10.0.2.13/24', s2, None)
+    addLinkWithIP(net, h11, '192.168.82.11/24', h92, '192.168.82.92/24')
+
+    # Public network
+    addLinkWithIP(net, h21, '10.0.3.21/24', s3, None)
+    addLinkWithIP(net, h22, '10.0.3.22/24', s3, None)
+    addLinkWithIP(net, h23, '10.0.3.23/24', s3, None)
+    addLinkWithIP(net, h21, '192.168.83.21/24', h93, '192.168.83.93/24')
+
+    # ISP network
+    addLinkWithIP(net, h91, '192.168.70.91/24', s4, None)
+    addLinkWithIP(net, h92, '192.168.70.92/24', s4, None)
+    addLinkWithIP(net, h93, '192.168.70.93/24', s4, None)
 
     net.start()
     print "Dumping host connections"
